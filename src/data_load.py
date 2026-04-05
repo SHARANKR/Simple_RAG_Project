38a7146e52b5016aefc659cb547f451e3c96e08c
SHARANKR <sharankr798@gmail.com>
2026-04-05 15:12:07 +0530

    1. Data_ingestion_through_yt

---
+++ src/data_load.py
@@ -0,0 +1,15 @@
+from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
+
+video_id = "Gfr50f6ZBvo" # only the ID, not full URL
+try:
+    # If you don’t care which language, this returns the “best” one
+    transcript_list = YouTubeTranscriptApi.get_transcript(video_id, languages=["en"])
+
+    # Flatten it to plain text
+    transcript = " ".join(chunk["text"] for chunk in transcript_list)
+    print(transcript)
+
+except TranscriptsDisabled:
+    print("No captions available for this video.")
+
+transcript_list
