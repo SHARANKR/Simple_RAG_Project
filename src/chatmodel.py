63b9594cf448842dd49dfd4f51f086e1a18b3fef
SHARANKR <sharankr798@gmail.com>
2026-04-05 16:16:47 +0530

    5. Chatmodel is set up

---
+++ src/chatmodel.py
@@ -0,0 +1,27 @@
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.2)
+
+prompt = PromptTemplate(
+    template="""
+      You are a helpful assistant.
+      Answer ONLY from the provided transcript context.
+      If the context is insufficient, just say you don't know.
+
+      {context}
+      Question: {question}
+    """,
+    input_variables = ['context', 'question']
+)
+
+question          = "is the topic of nuclear fusion discussed in this video? if yes then what was discussed"
+retrieved_docs    = retriever.invoke(question)
+
+retrieved_docs
+
+context_text = "\n\n".join(doc.page_content for doc in retrieved_docs)
+context_text
+
+final_prompt = prompt.invoke({"context": context_text, "question": question})
+
+final_prompt
