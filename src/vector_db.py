da3db77f079d53f88a52cb7b297b0cb23043ab93
SHARANKR <sharankr798@gmail.com>
2026-04-05 15:18:22 +0530

    2.1 Storing in the Vectordb

--- src/vector_db.py
+++ src/vector_db.py
@@ -1,6 +1,5 @@
+from langchain_community.vectorstores import FAISS
+
 embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
 vector_store = FAISS.from_documents(chunks, embeddings)
 
-vector_store.index_to_docstore_id
-
-vector_store.get_by_ids(['2436bdb8-3f5f-49c6-8915-0c654c888700'])
